348a2dc4ada066b2cd0964b6bbe50a0f554df4c0
andrew <contact@andrzejpakula.pl>
2016-06-08 12:55:35 +0200

    Kontroler obsługujący sekcje artykułów

---
+++ src/TmBundle/Controller/WikiController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use AppBundle\Entity\WikiArticle;
+use AppBundle\Entity\WikiHistory;
+use AppBundle\Form\Type\WikiArticleType;
+
+class WikiController extends Controller {
+
+    
+/**
+* @Route(
+ *     "/show_article/{title}",
+ *     name="wiki_show_article"
+ * )
+* @param wikiarticle $article
+* @return \Symfony\Component\HttpFoundation\Response
+*/
+
+public function showArticleAction(WikiArticle $article=null) {
+    if(!$article)  {
+        return $this->render('AppBundle:Wiki:error_article_not_found.html.twig');
+    }
+    return $this->render('AppBundle:Wiki:wiki_article.html.twig',
+        ['article' => $article]
+    );
+}
+
+ /**
+  * @Route(
+  *     "/add-article",
+  *     name="wiki_add_article"
+  * )
+  * @param Request $request
+  */
+
+public function addArticleAction(Request $request) {
+    $article = new WikiArticle();
+    $form = $this->createForm(WikiArticleType::class, $article);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+
+        if($form->get('preview')->isClicked()) {
+
+        return $this->render('AppBundle:Wiki:wiki_article.html.twig', array(
+            'article' => $article));
+        }
+
+        elseif($form->get('save')->isClicked()) {
+            $user = $this->getUser();
+            $article->setAuthor($user);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+            return $this->redirectToRoute('article_page_tm');
+        }
+
+    }
+
+    return $this->render('AppBundle:Wiki:add_article.html.twig', array(
+        'form' => $form->createView()
+    ));
+}
+
+    /**
+    * @Route(
+     *     "/edit-article/{title}",
+     *     name="wiki_edit_article"
+     * )
+    * @param Request $request
+    * @param wikiarticle $article
+    * @return \Symfony\Component\HttpFoundation\Response
+    */
+
+public function editArticleAction(Request $request, WikiArticle $article=null) {
+    if(!$article) {
+        return $this->render('AppBundle:Wiki:error_article_not_found.html.twig');
+    }
+
+    $form = $this->createForm(WikiArticleType::class, $article);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+
+        if($form->get('preview')->isClicked()) {
+
+            return $this->render('AppBundle:Wiki:wiki_article.html.twig', array(
+            'article' => $article));
+        }
+        elseif($form->get('save')->isClicked()) {
+
+            $history = new WikiHistory();
+            $history->setUser($this->getUser())
+            ->setWikiarticle($article)
+            ->setDate(new \DateTime('now'));
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->persist($history);
+            $em->flush();
+
+            return $this->redirectToRoute('article_page_tm');
+        }
+
+    }
+    return $this->render('AppBundle:Wiki:add_article.html.twig', array(
+    'form' => $form->createView()));
+}
+
+}
