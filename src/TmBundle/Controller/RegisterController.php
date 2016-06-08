de7439feb90221538ce0dc77d7d53724e2c0c246
andrew <contact@andrzejpakula.pl>
2016-06-08 21:42:31 +0200

    Dodanie funkcji wstawiania domyślnych rekordów do bazy po rejestracji

--- src/TmBundle/Controller/RegisterController.php
+++ src/TmBundle/Controller/RegisterController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AppBundle\Controller;
+namespace TmBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -9,9 +9,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Form\FormError;
 
-use AppBundle\Entity\User;
-use AppBundle\Form\Type\RegisterUserType;
-use AppBundle\Exception\UserException;
+use TmBundle\Entity\User;
+use TmBundle\Entity\ThemeUser;
+use TmBundle\Entity\DataUser;
+use TmBundle\Form\Type\RegisterUserType;
+use TmBundle\Exception\UserException;
 
 
 class RegisterController extends Controller {
@@ -26,40 +28,63 @@ class RegisterController extends Controller {
      */
     public function registerAction(Request $request) {
         $user = new User();
+        $data = new DataUser();
+        $theme = new ThemeUser();
+
         $registerUserForm = $this->createForm(RegisterUserType::class, $user);
 
-            if ($request->isXmlHttpRequest() && $request->isMethod('POST')) {
-                $registerUserForm->handleRequest($request);
+        if ($request->isXmlHttpRequest() && $request->isMethod('POST')) {
+            $registerUserForm->handleRequest($request);
 
-                if ($registerUserForm->isValid()) {
-                    try{
-                        $userManager = $this->get('user_manager');
-                        $userManager->registerUser($User);
+            if ($registerUserForm->isValid()) {
+                try{
+                    $userManager = $this->get('user_manager');
+                    $userManager->registerUser($user);
 
-                        $this->get('session')->getFlashBag()->add('success', 'Konto zostało utworzone. Na Twoją skrzynkę pocztową została wysłana wiadomość aktywacyjna.');
+                    $theme->setIdUser($user);
+                    $theme->setTheme('dark-grey');
+                    $theme->setFont('blue-white');
+                    $theme->setBackground('binding_dark');
 
-                    }
-                    catch (UserException $exc) {
-                        $this->get('session')->getFlashBag()->add('error', $exc->getMessage());
-                    }
+                    $em = $this->getDoctrine()->getManager();
+                    $em->persist($theme);
+                    $em->flush();
+
+                    $data->setIdUser($user);
+                    $data->setName(null);
+                    $data->setLastName(null);
+                    $data->setPhone(null);
+                    $data->setWww(null);
+                    $data->setAbout(null);
+
+                    $em = $this->getDoctrine()->getManager();
+                    $em->persist($data);
+                    $em->flush();
+
+                    $this->get('session')->getFlashBag()->add('success', 'Konto zostało utworzone. Na Twoją skrzynkę pocztową została wysłana wiadomość aktywacyjna.');
+
+                }
+                catch (UserException $exc) {
+                    $this->get('session')->getFlashBag()->add('error', $exc->getMessage());
+                }
+            }
+            else {
+
+                $secondPswd = $registerUserForm->get('plainPassword')->get('second')->getData();
+
+                if(null == $secondPswd) {
+                    $registerUserForm->get('plainPassword')->get('second')->addError(new FormError('Ta wartość nie powinna być pusta.'));
                 }
                 else {
-
-                    $secondPswd = $registerUserForm->get('plainPassword')->get('second')->getData();
-
-                    if(null == $secondPswd) {
-                        $registerUserForm->get('plainPassword')->get('second')->addError(new FormError('Ta wartość nie powinna być pusta.'));
-                    }
-                    else {
-                        $registerUserForm->get('plainPassword')->get('second')->addError(new FormError(''));
-                    }
-
-                    return [
-                        'registerUserForm' => $registerUserForm->createView()
-                    ];
+                    $registerUserForm->get('plainPassword')->get('second')->addError(new FormError(''));
                 }
 
+                return [
+                    'registerUserForm' => $registerUserForm->createView()
+                ];
             }
+
+        }
 
         return [
             'registerUserForm' => $registerUserForm->createView()
