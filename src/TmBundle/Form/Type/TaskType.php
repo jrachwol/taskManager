9b5d441d4dc746bc00a58ce273016b39fbcc5799
andrew <contact@andrzejpakula.pl>
2016-06-08 13:18:56 +0200

    Typ formularza dla dodawania i edycji zadań

---
+++ src/TmBundle/Form/Type/TaskType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+            ->add('title', TextType::class, array(
+                'label'=>"Tytuł"
+            ))
+            ->add('description', TextareaType::class, array(
+                'label'=>"Opis"
+            ))
+            ->add('level', ChoiceType::class, array(
+                'label'=>'Poziom',
+                'choices'=>array(
+                    'basic'=>'basic',
+                    'advanced'=>'advanced',
+                    'master'=>'master'
+                )
+            ))
+            ->add('save', SubmitType::class, array(
+                'label'=>"Zapisz"
+            ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Task'
+        ));
+    }
+
+}
