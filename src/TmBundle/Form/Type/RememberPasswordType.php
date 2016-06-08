ecf7038577a42ec2f8db8458f4f5e9935b325115
andrew <contact@andrzejpakula.pl>
2016-06-08 13:18:01 +0200

    Typ formularza dla przypominania hasła

---
+++ src/TmBundle/Form/Type/RememberPasswordType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+class RememberPasswordType extends AbstractType {
+
+    public function identity() {
+        return new Response("true");
+    }
+    
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+            ->add('email', EmailType::class, array(
+                'label' => 'Twój email',
+                'constraints' => array(
+                    new Assert\NotBlank(),
+                    new Assert\Email()
+                )
+            ))
+            ->add('submitRem', SubmitType::class, array(
+                'label' => 'Wyślij'
+            ));
+    }
+
+
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User',
+            'validation_groups' => array('rememberPswd')
+
+        ));
+    }
+
+}
