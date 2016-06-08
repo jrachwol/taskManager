ca5aeef50eaa9631c1a43d6ba781cfacd9c65e30
andrew <contact@andrzejpakula.pl>
2016-06-08 13:08:42 +0200

    Dodanie encji motywów użytkowników

---
+++ src/TmBundle/Entity/ThemeUser.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * @ORM\Table(name="theme_user")
+ * @ORM\Entity()
+ */
+class ThemeUser {
+
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+	private $id;
+
+
+    /**
+     * @ORM\ManyToOne(
+     *     targetEntity="User"
+     * )
+     * @ORM\JoinColumn(
+     *     name="user_id",
+     *     referencedColumnName="id",
+     *     nullable = true
+     * )
+     *
+     */
+    private $idUser;
+
+    /**
+     * @ORM\Column(type="string", length = 20, nullable = true)
+     *
+     */
+    private $theme;
+
+
+    /**
+     * @ORM\Column(type="string", length = 15, nullable = true)
+     *
+     */
+	private $font;
+
+
+    /**
+     * @ORM\Column(type="string", length = 15, nullable = true)
+     *
+     */
+    private $background;
+
+///////////////////////////////////////
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set theme
+     *
+     * @param string $theme
+     *
+     * @return ThemeUser
+     */
+    public function setTheme($theme)
+    {
+        $this->theme = $theme;
+
+        return $this;
+    }
+
+    /**
+     * Get theme
+     *
+     * @return string
+     */
+    public function getTheme()
+    {
+        return $this->theme;
+    }
+
+    /**
+     * Set font
+     *
+     * @param string $font
+     *
+     * @return ThemeUser
+     */
+    public function setFont($font)
+    {
+        $this->font = $font;
+
+        return $this;
+    }
+
+    /**
+     * Get font
+     *
+     * @return string
+     */
+    public function getFont()
+    {
+        return $this->font;
+    }
+
+    /**
+     * Set background
+     *
+     * @param string $background
+     *
+     * @return ThemeUser
+     */
+    public function setBackground($background)
+    {
+        $this->background = $background;
+
+        return $this;
+    }
+
+    /**
+     * Get background
+     *
+     * @return string
+     */
+    public function getBackground()
+    {
+        return $this->background;
+    }
+
+    /**
+     * Set idUser
+     *
+     * @param \AppBundle\Entity\User $idUser
+     *
+     * @return ThemeUser
+     */
+    public function setIdUser(\AppBundle\Entity\User $idUser = null)
+    {
+        $this->idUser = $idUser;
+
+        return $this;
+    }
+
+    /**
+     * Get idUser
+     *
+     * @return \AppBundle\Entity\User
+     */
+    public function getIdUser()
+    {
+        return $this->idUser;
+    }
+}
