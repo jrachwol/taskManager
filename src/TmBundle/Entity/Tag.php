96bfda2e2f67e0bea59e9601143f13abbdc659cb
andrew <contact@andrzejpakula.pl>
2016-06-08 13:05:43 +0200

    Dodanie encji dla tagów

---
+++ src/TmBundle/Entity/Tag.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="tag")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Tag {
+
+
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=120, unique=true)
+     */
+    private $name;
+
+
+    /**
+     * @ORM\Column(type="string", length=120, unique=true)
+     */
+    private $slug;
+
+
+    /**
+     * @ORM\ManyToMany(
+     *     targetEntity="Task",
+     *     mappedBy="tags"
+     * )
+     */
+    private $tasks;
+
+/////////////////////////////////////////////////
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->tasks = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Tag
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     *
+     * @return Tag
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = \AirBlogBundle\Libs\Url::sluggify($slug);
+        return $this;
+
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Add task
+     *
+     * @param \AppBundle\Entity\Task $task
+     *
+     * @return Tag
+     */
+    public function addTask(\AppBundle\Entity\Task $task)
+    {
+        $this->tasks[] = $task;
+
+        return $this;
+    }
+
+    /**
+     * Remove task
+     *
+     * @param \AppBundle\Entity\Task $task
+     */
+    public function removeTask(\AppBundle\Entity\Task $task)
+    {
+        $this->tasks->removeElement($task);
+    }
+
+    /**
+     * Get tasks
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getTasks()
+    {
+        return $this->tasks;
+    }
+
+/////////////////////////////////////////////////
+
+    /**
+     * @ORM\PrePersist
+     * @ORM\PreUpdate
+     */
+    public function preSave(){
+        if(null === $this->slug){
+            $this->setSlug($this->getName());
+        }
+    }
+}
